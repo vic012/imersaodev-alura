d895040e9555b95336a2483aa34a883c54653b64
Pedro Henrique <henriquevic012@gmail.com>
2021-03-23 18:22:41 -0300

    Evolução do pikachu

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-function moeda(){
-  var moeda = prompt('Digite um valor que deseja converter de Libra em Dólar');
-  var dolar = parseFloat(moeda);
-  var libra = 1.39;
-  var conversao = (dolar * libra).toFixed(2);
-  alert('£ ' + moeda + ' corresponde a ' + conversao + ' R$');
-}
-
-function temperatura(){
-  var temperatura = prompt('Digite um valor que deseja converter de graus Celsius para Fahrenheit');
-  var celsius = parseFloat(temperatura);
-  var fahrenheit = ((9 / 5) * celsius + 32).toFixed(2);
-  alert(temperatura + ' °C' + ' corresponde a ' + fahrenheit + ' °F');
+function evoluir() {
+  var evolucao = parseInt(prompt('Qual evolução você deseja visualizar?'));
+  
+  if(evolucao === 1){
+    var pokemon = document.getElementsByClassName("pokemon");
+    pokemon[0].src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/172.png";
+    var nome = document.getElementsByClassName("nome");
+    nome[0].innerHTML = "Pichu";
+    
+  } else if(evolucao === 2){
+    var pokemon = document.getElementsByClassName("pokemon");
+    pokemon[0].src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png";
+    var nome = document.getElementsByClassName("nome");
+    nome[0].innerHTML = "Pikachu";
+  } else if(evolucao === 3){
+    var pokemon = document.getElementsByClassName("pokemon");
+    pokemon[0].src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/026.png";
+    var nome = document.getElementsByClassName("nome");
+    nome[0].innerHTML = "Raichu";
+  } else {
+    alert('O Pikachu só tem 3 evoluções!')
+  }
+  
 }
